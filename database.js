4e084d4d1fe231e9e65ff16aaa6cb0d8d30b9aa2
Mark Buskbjerg <mark.buskbjerg@gmail.com>
2023-11-07 19:54:38 +0100

    Connecting to a cloud MongoDB

--- database.js
+++ database.js
@@ -1,17 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = () => {
-	mongoose
-		.connect(process.env.DB_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		})
-		.then(() => {
-			console.log('MongoDB connected successfully');
-		})
-		.catch((err) => {
-			console.error('Error connecting to MongoDB: ', err);
-		});
+  mongoose
+    .connect(process.env.DB_URI, {})
+    .then(() => {
+      console.log("MongoDB connected successfully");
+    })
+    .catch((err) => {
+      console.error("Error connecting to MongoDB: ", err);
+    });
 };
 
 module.exports = connectDB;
